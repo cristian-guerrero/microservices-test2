ca488d992647d9bdf44cdc8df0c7705fa82f3cff
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-15 09:26:03 -0500

    added mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 import { currentUserRouter, signInRoute, signOutRoute, signUpRoute } from './routes'
 import { NotFoundError } from './errors/not-found.error';
@@ -11,8 +12,8 @@ const app = express()
 app.use(json())
 
 app.use((req, res, next) => {
-    next()
-    console.log('request to: ', req.url)
+  next()
+  console.log('request to: ', req.url)
 
 })
 
@@ -22,13 +23,35 @@ app.use(signInRoute)
 app.use(signOutRoute)
 app.use(signUpRoute)
 
-app.all('*', async (req, res, next ) => {
-    next (new NotFoundError())
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError())
 })
-
 
 app.use(errorHandler)
-app.listen(3000, () => {
 
-    console.log('Auth server listening on localhost:3000')
-})
+const start = async () => {
+  const url = 'mongodb://auth-mongo-clusterip-srv:27017/auth'
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+
+    console.log('connected to: ', url)
+
+    app.listen(3000, () => {
+
+      console.log('Auth server listening on localhost:3000')
+    })
+
+  } catch (err) {
+
+    console.error(err)
+  }
+
+}
+
+
+start()
+
