d2e1b74806dfba26a71ba130f9a22df818325f8b
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-15 16:17:46 -0500

    added cookie config to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { json } from 'body-parser'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session' 
 
 import { currentUserRouter, signInRoute, signOutRoute, signUpRoute } from './routes'
 import { NotFoundError } from './errors/not-found.error';
@@ -8,8 +9,16 @@ import { errorHandler } from './middlewares/error-handler'
 
 
 const app = express()
+// because express is behind  proxy 
+app.set('trust proxy', true)
 
 app.use(json())
+
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+
+}))
 
 app.use((req, res, next) => {
   next()
