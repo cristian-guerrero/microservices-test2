95784dec0c016fd7f3234092ae39f1be132c3687
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-15 00:18:38 -0500

    added next function and not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,8 @@ import express from 'express'
 import { json } from 'body-parser'
 
 import { currentUserRouter, signInRoute, signOutRoute, signUpRoute } from './routes'
-import{ errorHandler} from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found.error';
+import { errorHandler } from './middlewares/error-handler'
 
 
 const app = express()
@@ -11,8 +12,8 @@ app.use(json())
 
 app.use((req, res, next) => {
     next()
-    console.log('request to: ',req.url)
-    
+    console.log('request to: ', req.url)
+
 })
 
 // add routes
@@ -21,9 +22,13 @@ app.use(signInRoute)
 app.use(signOutRoute)
 app.use(signUpRoute)
 
+app.all('*', async (req, res, next ) => {
+    next (new NotFoundError())
+})
+
 
 app.use(errorHandler)
 app.listen(3000, () => {
 
-    console.log('Auth server listening on localhost:3000') 
+    console.log('Auth server listening on localhost:3000')
 })
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator'
 
 import { DatabaseConnectionError, RequestValidationError } from '../errors'
@@ -9,7 +9,7 @@ const router = Router()
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid..'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password mus be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response, next : NextFunction) => {
 
     const errors = validationResult(req)
 
@@ -17,10 +17,10 @@ router.post('/api/users/signup', [
 
 
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array())
+        next (new RequestValidationError(errors.array()))
     }
     
-    throw new DatabaseConnectionError()
+    next( new DatabaseConnectionError())
     //res.send('User created')
 
 })
