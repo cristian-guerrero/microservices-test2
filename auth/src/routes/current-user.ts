c7c0f6fc32aba6b8887648a2cd46202140869c6e
Cristian Guerrero <guerrero.chavez.cristia@gmail.com>
2020-11-15 21:34:11 -0500

    current user functionality

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,30 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
+const JWT_KEY = process.env.JWT_KEY!
 
 router.get('/api/users/currentuser', (req, res) => {
 
-    res.send('Response')
+
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+
+  try{
+    
+    const payload = jwt.verify(req.session.jwt, JWT_KEY)
+
+
+    res.send({currentUser: payload})
+
+    
+  }catch(err) {
+    return res.send({ currentUser: null })
+  }
+
 })
 
-export { router as currentUserRouter }
+export { router as currentUserRouter } 
