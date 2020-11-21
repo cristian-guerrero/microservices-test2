c6343abf68fb3b6958afafbb5cd0983739e316e8
cristian-guerrero <guerrero.chavez.cristian@gmail.com>
2020-11-21 18:46:26 -0500

    added headers in te server side request

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,18 +4,33 @@ import axios from 'axios'
 /**
  * 
  */
-const Index = ({currentUser}) => {
+const Index = ({ currentUser }) => {
 
   console.log('In the Index component  -> ', currentUser)
   return <h1>Index page</h1>
 }
 
 
-Index.getInitialProps = async  () => {
+Index.getInitialProps = async ({ req }) => {
 
-  const response = await axios.get('htpps://ingress-nginx/api/users/currentuser')
-  console.log('on the server!')
+  let response
 
+  console.log('getInitialProps executed!')
+
+  if (typeof window === 'undefined') {
+    const { headers } = req
+    // console.log(headers)
+    response = await axios.get('htpp://ingress-nginx/api/users/currentuser', {
+      headers: {
+        // agregar este header para que ingress-ngix separa para donde va la petición
+        // Host: 'ticketing.dev'
+        // pasar todos los headers que llegan del navegador (incluida la cookie)
+        ...headers
+      }
+    })
+  } else {
+    response = await axios.get('/api/users/currentuser')
+  }
   return response.data
 }
 
