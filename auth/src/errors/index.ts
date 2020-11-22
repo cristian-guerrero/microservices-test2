c40ac575f8af9bb64df7452624063827e1736dec
cristian-guerrero <guerrero.chavez.cristian@gmail.com>
2020-11-22 14:37:56 -0500

    moved common functionality to common project

--- auth/src/errors/index.ts
+++
@@ -1,6 +0,0 @@
-import { DatabaseConnectionError } from './database-connecton.error';
-import { RequestValidationError } from './request-validaton.error';
-import {BAdRequestError } from './bad-request.error'
-
-
-export {DatabaseConnectionError, RequestValidationError,BAdRequestError }
