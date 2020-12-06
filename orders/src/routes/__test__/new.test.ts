c5dc1710dc1f7f5d9c6d05cc894ad9a5059ce79f
cristian-guerrero <guerrero.chavez.cristian@gmail.com>
2020-12-05 20:04:07 -0500

    new test

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -2,6 +2,10 @@ import request from 'supertest'
 import app from '../../app'
 import mongoose from 'mongoose'
 import { signingTest } from '../../test/signup'
+import { Order } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+import { OrderStatus } from '@microservices-commons/common'
+
 
 
 it('returns an error if the ticket does not exist', async () => {
@@ -23,12 +27,46 @@ it('returns an error if the ticket does not exist', async () => {
 
 
 it('returns an error if the ticket is already reserved', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString()
+  const cookie = signingTest()
+
+  const ticket = await Ticket.create( {
+    title: 'Concert',
+    price: 30
+  })
+
+  const order = await Order.create( {
+    ticket,
+    userId: userId ,
+    status: OrderStatus.Created,
+    expirationAt: new Date()
+  })
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie',cookie)
+    .send({ticketId: ticket.id })
+    .expect(400)
+
 
 })
 
 
 it('reserves a ticket ', async () => {
 
+  const userId = mongoose.Types.ObjectId().toHexString()
+  const cookie = signingTest()
+
+  const ticket = await Ticket.create( {
+    title: 'Concert',
+    price: 30
+  })
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', cookie)
+    .send({ticketId: ticket.id })
+    .expect(201)
 })
 
 
