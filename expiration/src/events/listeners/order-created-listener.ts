a793f975d6a9e164e33609f7f11efcac46339fb4
cristian-guerrero <guerrero.chavez.cristian@gmail.com>
2020-12-10 21:08:23 -0500

    added order create listener in index

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,18 +1,22 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@microservices-commons/common'
 import { Message } from 'node-nats-streaming'
 import { queueGroupName } from './queue-group-name'
+import { expirationQueue } from '../../queues/expiration-queue'
 
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
-  queueGroupName =  queueGroupName
+  queueGroupName = queueGroupName
   subject: Subjects.OrderCreated = Subjects.OrderCreated
 
+  async onMessage(parsedData: OrderCreatedEvent['data'], msg: Message) {
 
-  async onMessage(parsedData: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderId: parsedData.id
+    })
 
+    msg.ack()
 
   }
-
 
 }
