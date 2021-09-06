39faf5940bb8b3eeab8fafa787c25d3f0232db89
Cristian Guerrero <guerrer.chavez.cristian@gmail.com>
2021-09-06 00:02:51 -0500

    update ingress

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,11 +5,13 @@ import axios from 'axios'
  * @param {*} param0 
  */
 const buildClient = ({req}) => {
+  
+  // console.log('build client----------------------------------------------------------')
 
   if( typeof window === 'undefined') {
 
     return axios.create({
-      baseURL: 'htpp://ingress-nginx',
+      baseURL: 'http://ingress-nginx',
       headers: req.headers
     })
   }else  {
